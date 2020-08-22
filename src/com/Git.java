926eed9af1d8221fbec7d34fabcbc63fb4a9509e
SPOTO-TianMa <68541236+SPOTO-TianMa@users.noreply.github.com>
2020-08-22 21:21:34 +0800

    Update Git.java

--- src/com/Git.java
+++ src/com/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("第一条代码");
         System.out.println("第二条代码");
+        System.out.println("第三条代码");
     }
 }
